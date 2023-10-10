b37f2da4f93e46fd8e243a73da33b8863ae805e3
hanh-gamejam <95898223+hanh-gamejam@users.noreply.github.com>
2023-10-10 11:00:49 +0000

    Update example_test.go

--- example_test.go
+++ example_test.go
@@ -7,8 +7,8 @@ import (
 	"os"
 	"time"
 
-	"github.com/Rican7/conjson"
-	"github.com/Rican7/conjson/transform"
+	"github.com/superfineorg/conjson"
+	"github.com/superfineorg/conjson/transform"
 )
 
 type exampleModel struct {
